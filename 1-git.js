8444ac1d683055988eacf7dad5890c0bc8d8b160
yongjie tian <t2028000599@163.com>
2022-12-11 18:49:39 +0800

    修改文件信息

--- 1-git.js
+++ 1-git.js
@@ -1,4 +1,13 @@
 //git init 初始化一个git项目。
 //git status 查看当前git版本库的内容。
 //git add   将文件添加到git里面。
-//git add . 将所有文件添加到git里面。
+//git add . 将当前目录的所有文件添加到git的暂存区里面。
+//版本库   暂存区
+//git commit -m '初始化git项目'  提交到版本库master，后面的这个写的详细一些，不然到时候就看不懂了。信息，有一定的规范。
+//git diff  再终端中查看修改了当前的哪些内容,还没有提交到暂存区。
+//git log 指向当前修改的作者。版本号,就是日志。时间。
+//git log -p  具体查看再什么时间干了什么事情。
+//git reset --herd  版本号    版本回滚,
+//git log --pretty=oneline 只查看版本，其他不出来。
+//git reset --herd HEAD^ 回到上一个版本，没必要记，用的不多。
+//
