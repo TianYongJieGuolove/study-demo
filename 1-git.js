089153bb79b250dc8a1eb6ddf627cf0966522e0a
yongjie tian <t2028000599@163.com>
2022-12-11 19:26:58 +0800

    跟行

--- 1-git.js
+++ 1-git.js
@@ -7,7 +7,10 @@
 //git diff  再终端中查看修改了当前的哪些内容,还没有提交到暂存区。
 //git log 指向当前修改的作者。版本号,就是日志。时间。
 //git log -p  具体查看再什么时间干了什么事情。
-//git reset --herd  版本号    版本回滚,
+//git reset --hard  版本号    版本回滚,
 //git log --pretty=oneline 只查看版本，其他不出来。
 //git reset --herd HEAD^ 回到上一个版本，没必要记，用的不多。
-//
+//git reflog  当前所在的版本。记录每一次命令操作,隔日回滚。
+//git store --stage index.html 再暂存区撤销修改,还没有进行提交。
+//git reset --soft HEAD^  撤销当前版本库的提交。撤销上一次的版本库提交。
+//git reflog 查看当前所有的操作。
